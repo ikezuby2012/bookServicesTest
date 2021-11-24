62102defc9250d46b4be3060ad10b09b826047b9
zedxog <zubydev2012@gmail.com>
2021-11-25 00:02:27 +0100

    added a change

--- app.js
+++ app.js
@@ -46,25 +46,41 @@ const bookService = (function () {
         },
         borrowBook: function (name) {
             //check if book exist
-            _book = books.filter(book => book.name === name);
+            for (let book of books) {
+                if (book.name === name) {
+                    _book = book;
+                }
+            }
+            console.log(_book);
+
             if (_book) {
                 borrowedBooks.push(_book);
                 console.log("you have just borrowed a book!");
                 // remove from store
                 books = books.filter(book => book.name !== name);
+                console.log("borrowed books");
+                console.log(borrowedBooks);
+                return borrowedBooks;
             } else {
                 console.log("book does not exist!");
             }
         },
         returnBorrowedBook: function (name) {
             // check if book exist in borrowedbooks array
-            _book = borrowedBooks.map(book => book.name === name);
+            for (let book of books) {
+                if (book.name === name) {
+                    _book = book;
+                }
+            }
             // if it exists
             if (_book) {
                 //then remove book from borrowedBooks
                 _book = borrowedBooks.filter(book => book.name !== name);
+                console.log("you just returned a borrowed book!");
                 //add to books
                 books.push(_book);
+                console.log(borrowedBooks);
+                return borrowedBooks;
             } else {
                 console.log("operation failed!")
             }
@@ -78,8 +94,13 @@ const newBook3 = bookService.createNewBook("test3", "testAuthor3", 2003);
 const newBook4 = bookService.createNewBook("test4", "testAuthor4", 2004);
 const newBook5 = bookService.createNewBook("test5", "testAuthor5", 2005);
 // bookService.getAllBooks();
-bookService.getBookByName("test5");
+// bookService.getBookByName("test5");
+bookService.borrowBook("test2");
+bookService.borrowBook("test4");
+
+bookService.returnBorrowedBook("test4");
 module.exports = {
     book: book,
+    books,
     bookService
 }
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-const { book, bookService } = require("./app");
+const { books, book, bookService } = require("./app");
 
-// test("create a new book", () => {
-//     expect(new book("zuby", "hello", 2017)).toBe()
-// })
-
-test("create a new book", () => {
-    expect(bookService.createNewBook()).not.toBeNull();
+test("books array exists", () => {
+    expect(books).toBeDefined();
 })
+
+test("book services exist", () => {
+    expect(bookService).toBeDefined();
+});
 
 test("create a new book", () => {
     expect(bookService.createNewBook("underwater", "tom peters", 2017)).toEqual({
@@ -22,9 +22,31 @@ test("get all books", () => {
     const newBook3 = bookService.createNewBook("test3", "testAuthor3", 2003);
     const newBook4 = bookService.createNewBook("test4", "testAuthor4", 2004);
     const newBook5 = bookService.createNewBook("test5", "testAuthor5", 2005);
-    expect(bookService.getAllBooks()).toEqual();
+    expect(bookService.getAllBooks()).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining(newBook)
+        ])
+    );
 });
 
 test("get a book by name", () => {
-    
+    expect(bookService.getBookByName("test5")).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({ name: 'test5', author: 'testAuthor5', yearOfPub: 2005 })
+        ]));
+})
+
+test("borrow book", () => {
+    expect(bookService.borrowBook("test5")).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({ name: 'test5', author: 'testAuthor5', yearOfPub: 2005 })
+        ]));
+});
+
+test("return borrowed book", () => {
+    expect(bookService.returnBorrowedBook).toBeDefined();
+    expect(bookService.returnBorrowedBook("test5")).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({ name: 'test5', author: 'testAuthor5', yearOfPub: 2005 })
+        ]));
 })
